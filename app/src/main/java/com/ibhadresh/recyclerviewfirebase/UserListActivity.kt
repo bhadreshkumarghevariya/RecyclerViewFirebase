908711570e1722bccbf8f76c613d31ecc2178d16
bhadreshkumarghevariya <bhadreshkumarghevariya@gmail.com>
2023-03-12 17:12:23 -0400

    Recycler View complete with data from firebase.

--- app/src/main/java/com/ibhadresh/recyclerviewfirebase/UserListActivity.kt
+++ app/src/main/java/com/ibhadresh/recyclerviewfirebase/UserListActivity.kt
@@ -2,10 +2,44 @@ package com.ibhadresh.recyclerviewfirebase
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.google.firebase.database.*
 
 class UserListActivity : AppCompatActivity() {
+    private lateinit var dbref :DatabaseReference
+    private lateinit var userRecyclerView: RecyclerView
+    private lateinit var userArrayList : ArrayList<User>
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_user_list)
+
+        userRecyclerView = findViewById(R.id.userList)
+        userRecyclerView.layoutManager = LinearLayoutManager(this)
+        userRecyclerView.setHasFixedSize(true)
+
+        userArrayList = arrayListOf<User>()
+        getUserData()
+    }
+
+    private fun getUserData() {
+        dbref = FirebaseDatabase.getInstance().getReference("Users")
+
+        dbref.addValueEventListener(object : ValueEventListener{
+            override fun onDataChange(snapshot: DataSnapshot) {
+                if(snapshot.exists()){
+                    for(userSnapShot in snapshot.children){
+                        val user = userSnapShot.getValue(User::class.java)
+                        userArrayList.add(user!!)
+                    }
+                    userRecyclerView.adapter = UserAdapter(userArrayList)
+                }
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                TODO("Not yet implemented")
+            }
+
+        })
     }
 }
