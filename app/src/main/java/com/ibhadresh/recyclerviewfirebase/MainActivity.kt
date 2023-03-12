c5baf64cdc79cac89615a0d1f8da438e19f83ad7
bhadreshkumarghevariya <bhadreshkumarghevariya@gmail.com>
2023-03-12 16:09:09 -0400

    Added a button on main activity to start recycler view activity.

--- app/src/main/java/com/ibhadresh/recyclerviewfirebase/MainActivity.kt
+++ app/src/main/java/com/ibhadresh/recyclerviewfirebase/MainActivity.kt
@@ -1,11 +1,22 @@
 package com.ibhadresh.recyclerviewfirebase
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val btnShowRecycleView = findViewById<Button>(R.id.btnShowRecycleView)
+        btnShowRecycleView.setOnClickListener{
+            val intent = Intent(this, UserListActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
